2586d8658f3123c639823947ff75adf1f4f432ca
Luigi De Russis <luigidr@gmail.com>
2026-04-20 18:09:04 +0200

    add today's exercise

---
+++ week08/react-qa-complete/src/components/AnswerForm.jsx
@@ -0,0 +1,60 @@
+import dayjs from "dayjs";
+import { useActionState } from "react";
+import { Alert, Button, Form } from "react-bootstrap";
+
+function AnswerForm(props) {
+  const initialState = {
+    text: props.answer?.text,
+    email: props.answer?.author.email,
+    date: props.answer?.date ?? dayjs()
+  };
+
+  const handleSubmit = async (prevState, formData) => {
+    // creare un oggetto dal formData
+    const answer = Object.fromEntries(formData.entries());
+
+    // esempio di validazione
+    if(answer.text.trim()==="") {
+      answer.error = "The answer can't be empty, please fix it!"
+      answer.date = dayjs(answer.date);
+      return answer;
+    }
+
+    if(props.addAnswer)
+      props.addAnswer(answer);
+    else
+      props.editAnswer({id: props.answer.id, ... answer});
+
+
+    return initialState;
+  }
+
+  const [state, formAction] = useActionState(handleSubmit, initialState);
+
+  return(
+    <>
+      {state.error && <Alert variant="secondary">{state.error}</Alert>}
+      <Form action={formAction}>
+        <Form.Group>
+          <Form.Label>Text</Form.Label>
+          <Form.Control name="text" type="text" required={true} minLength={2} defaultValue={state.text}></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>e-mail</Form.Label>
+          <Form.Control name="email" type="email" required={true} defaultValue={state.email}></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Date</Form.Label>
+          <Form.Control name="date" type="date" required={true} defaultValue={state.date.format("YYYY-MM-DD")}></Form.Control>
+        </Form.Group>
+        {props.addAnswer && <Button variant="primary" type="submit">Add</Button>}
+        {props.editAnswer && <Button variant="success" type="submit">Edit</Button>}
+        {" "}
+        <Button variant="danger" onClick={props.cancel}>Cancel</Button>
+      </Form>
+    </>
+  );
+
+}
+
+export default AnswerForm;
---
+++ week08/react-qa/src/App.jsx
@@ -0,0 +1,63 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+
+import { Question, Answer } from "./models/QAModels.js";
+import NavHeader from "./components/NavHeader";
+import QuestionDescription from "./components/Description";
+import Answers from "./components/Answers";
+
+const myQuestion = new Question(1, "Is Javascript better than Python?", "luigi.derussis@polito.it", 1, "2026-03-30");
+
+myQuestion.answers = [
+  new Answer(1, "Yes, it is!", "luca.scibetta@polito.it", 2, "2026-03-30", 5),
+  new Answer(2, "No, it isn't!", "mario.rossi@mail.it"  , 3, "2026-03-30", -2),
+  new Answer(3, "It depends on the use case.", "claudia.bianchi@mail.it", 4, "2026-03-30", 0)
+];
+
+function App() {
+  const [question, setQuestion] = useState(myQuestion);
+  const [answers, setAnswers] = useState(myQuestion.answers);
+
+  const voteUp = (answerId) => {
+    setAnswers(oldAnswers => {
+      return oldAnswers.map(ans => {
+        if(ans.id === answerId)
+          return new Answer(ans.id, ans.text, ans.author.email, ans.author.id, ans.date, ans.score + 1);
+        else return ans;
+      });
+    });
+  }
+
+  const addAnswer = (answer) => {
+    setAnswers(oldAnswers => {
+      // temporaneo
+      const newId = Math.max(...oldAnswers.map(ans => ans.id)) + 1;
+      const newAnswer = new Answer(newId, answer.text, answer.email, undefined, answer.date);
+      return [...oldAnswers, newAnswer];
+    });
+  }
+
+  const updateAnswer = (answer) => {
+    setAnswers(oldAnswers => {
+      return oldAnswers.map(ans => {
+        if(ans.id === answer.id)
+          return new Answer(answer.id, answer.text, answer.email, ans.author.id, answer.date, ans.score);
+        else return ans;
+      });
+    });
+  }
+
+  return (
+    <>
+      <NavHeader questionNum={question.id} />
+      <Container fluid className="mt-3">
+        <QuestionDescription question={question} />
+        <Answers answers={answers} voteUp={voteUp} addAnswer={addAnswer} editAnswer={updateAnswer}/>
+      </Container>
+    </>
+  )
+
+}
+
+export default App;
